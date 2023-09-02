ff0e7c18a06b59cde785ed9a76f6f38397e3c4dc
Popolia <69745473+Popolia@users.noreply.github.com>
2023-09-02 23:47:57 +0200

    Create Recherche _Log_Palia.bat

---
+++ Recherche _Log_Palia.bat
@@ -0,0 +1,18 @@
+@echo off
+
+:: Chemin du fichier Palia.log
+set "logFilePath=C:\Users\Admin\AppData\Local\Palia\Saved\Logs\Palia.log"
+
+:: Vérifier si le fichier existe
+if exist "%logFilePath%" (
+    start "" "%logFilePath%"
+) else (
+    echo Le fichier Palia.log n'a pas été trouvé.
+    echo The Palia.log file was not found.
+)
+
+:: Attendre 2 secondes (ajustable)
+timeout /t 2 >nul
+
+:: Fermer automatiquement la fenêtre de l'invite de commande
+exit
